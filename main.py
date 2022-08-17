16db0d6fa9069852da91bc9f644b758443bf3a67
Catilena <otakunonekochan@gmail.com>
2022-08-18 00:51:39 +0200

    initial commit

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def create_tweet():
 
 
 def tweet_quote():
-    interval = 60*60*24
+    interval = 60*24
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
